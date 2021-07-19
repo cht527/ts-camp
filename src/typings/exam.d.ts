a6786e86f6b730f2731f3cddeaa9753b7c1991cd
caohaitao <cht527@126.com>
2021-07-19 16:24:58 +0800

    feat: add Chainable

--- src/typings/exam.d.ts
+++ src/typings/exam.d.ts
@@ -24,4 +24,31 @@ type D = LookUp<Animal, 'cat'>;
 type Check_LookUp = IsTypeEqual<D, Cat>
 // -----------------------------------------------
 
+// 2、 Chainable
 
+type Chainable<T = {}> = {
+  option: <K extends string, V>(key: K, value: V) => Chainable<T & {[P in K]: V}>,
+  get(): T
+}
+
+
+/* _____________ 测试用例 _____________ */
+
+declare const a: Chainable
+
+const result = a
+  .option('foo', 123)
+  .option('bar', { value: 'Hello World' })
+  .option('name', 'type-challenges')
+  .get()
+
+type Expected = {
+  foo: number
+  bar: {
+    value: string
+  }
+  name: string
+}
+
+
+type Check_Chainable = IsTypeEqual<typeof result, Expected>
