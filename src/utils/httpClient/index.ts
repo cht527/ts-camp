5c69e63e2f7de46f87cb9c6368c89443151b2e23
cht527 <cht527@126.com>
2021-11-18 16:43:59 +0800

    chore: httpClient add useCache

--- src/utils/httpClient/index.ts
+++ src/utils/httpClient/index.ts
@@ -6,25 +6,29 @@ import HttpInstance from './interceptors';
  export const getFetch = <T = any>(
      url: string,
      params?: any,
-     config?: AxiosRequestConfig 
+     config?: AxiosRequestConfig & { useCache?: boolean }
  ): Promise<T> => HttpInstance.get(url, { ...config, params });
  
  export const postFetch = <T = any>(
      url: string,
      params?: any,
-     config?: AxiosRequestConfig
+     config?: AxiosRequestConfig & { useCache?: boolean }
  ): Promise<T> => HttpInstance.post(url, params, config);
 
  export function wrapperGetFetch<P>(
      commonUrl: string,
      url: string,
-     config?: AxiosRequestConfig
+     config?: AxiosRequestConfig & { useCache?: boolean }
  ) {
      return <T>(params: T) => getFetch<P>(`${commonUrl}${url}`, params, config);
  }
  
- export function wrapperPostFetch<P>(commonUrl: string, url: string) {
-     return <T>(params: T) => postFetch<P>(`${commonUrl}${url}`, params);
+ export function wrapperPostFetch<P>(
+     commonUrl: string, 
+     url: string,
+     config?: AxiosRequestConfig & { useCache?: boolean }
+ ) {
+    return <T>(params: T) => postFetch<P>(`${commonUrl}${url}`, params, config);
  }
  
 
