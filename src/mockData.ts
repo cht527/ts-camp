d171e7c32c4b51d054560abe1187c46fe15e99b7
htCaoHaitao <95202481+htCaoHaitao@users.noreply.github.com>
2023-08-11 18:31:10 +0800

    feat: update nav config

--- src/mockData.ts
+++ src/mockData.ts
@@ -1,21 +1,37 @@
-import * as T from './interface/test';
+import * as T from "./interface/test";
 
+export const NAV_CONFIG: T.INavConfig[] = [
+  {
+    id: 0,
+    title: "首页",
+    slug: "",
+  },
+  {
+    id: 1,
+    title: "关于",
+    slug: "about",
+  },
+  {
+    id: 12,
+    title: "看板",
+    slug: "dashboard",
+  },
+];
 
+const MOCK_DATA: T.ITest = {
+  rgb: {
+    r: 1,
+    g: 1,
+    b: 1,
+  },
+  blackWhite: {
+    black: "S",
+    white: "D",
+  },
+  data: {
+    a: T.Data.P1,
+    b: T.Data.P2,
+  },
+};
 
-const MOCK_DATA:T.ITest={
-    rgb:{
-        r:1,
-        g:1,
-        b:1
-    },
-    blackWhite: {
-        black: 'S',
-        white: 'D'
-    },
-    data:{
-        a: T.Data.P1,
-        b: T.Data.P2
-    }
-}
-
-export default MOCK_DATA
+export default MOCK_DATA;
